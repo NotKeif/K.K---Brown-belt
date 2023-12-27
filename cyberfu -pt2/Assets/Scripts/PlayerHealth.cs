f0044d7920727209dc87b1421afedbd249807d79
CNMarkham <131494806+NotKeif@users.noreply.github.com>
2023-12-27 16:51:18 -0500

    yay
    dsads

---
+++ cyberfu -pt2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentPlayerHealth = 10;
+    public int enemyDamage = 2;
+
+    public PlayerExplosionParticles particles;
+
+    private Animator playerAnimator;
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        particles = GetComponent<PlayerExplosionParticles>();
+    }
+
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamage;
+
+        if (currentPlayerHealth <= 0)
+        {
+            particles.Explode(); 
+            Invoke("ReloadScene", 5);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "HitCollider")
+        {
+            HurtPlayer();
+            playerAnimator.SetTrigger("Hit");
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene("CyberFu");
+    }
+
+}
