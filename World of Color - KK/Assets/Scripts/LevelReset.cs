28451d64f14e12b304035243ce276fe0ca740765
Kelton <Keithmarkham430@gmail.com>
2024-01-03 16:46:43 -0500

    doan
    sqiosaj

---
+++ World of Color - KK/Assets/Scripts/LevelReset.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour
+{
+    public GameObject player;
+    public ParticleSystem explosion;
+    public ParticleSystem trail;
+    void Start()
+    {
+        explosion.Stop();
+        trail.Play();
+    }
+    void FixedUpdate()
+    {
+        explosion.transform.position = player.transform.position;
+        trail.transform.position = player.transform.position;
+    }
+    public void GameOver()
+    {
+        player.SetActive(false);
+        Invoke("Died", 2);
+        explosion.Play();
+        trail.Stop();
+    }
+
+    void Reload()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void Died()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
+
