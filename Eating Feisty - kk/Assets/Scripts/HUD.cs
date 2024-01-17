b57ee0ba26cd09b820d74088edb7b208ca7d968a
Roosham <131494806+NotKeif@users.noreply.github.com>
2024-01-17 17:51:24 -0500

    ALMOST THERE
    STEP 8E

---
+++ Eating Feisty - kk/Assets/Scripts/HUD.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Level level;
+
+    public Text remainingText;
+    public Text remainingSubtext;
+    public Text targetText;
+    public Text targetSubtext;
+    public Text scoreText;
+
+    public Image[] stars;
+
+    private int starIndex;
+    private bool isGameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
